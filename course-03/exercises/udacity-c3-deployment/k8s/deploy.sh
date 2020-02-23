50056a5476cc9abeba564ce10764f3d28b718576
jipsterious <cwtien@gmail.com>
2020-02-24 00:10:08 +0800

    travis push

--- course-03/exercises/udacity-c3-deployment/k8s/deploy.sh
+++ course-03/exercises/udacity-c3-deployment/k8s/deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+kubectl apply -f aws-secret.yaml
+kubectl apply -f env-configmap.yaml
+kubectl apply -f env-secret.yaml
+kubectl apply -f backend-feed-service.yaml
+kubectl apply -f backend-user-service.yaml
+kubectl apply -f frontend-service.yaml
+kubectl apply -f reverseproxy-service.yaml
 kubectl apply -f backend-feed-deployment.yaml 
 kubectl apply -f backend-user-deployment.yaml 
 kubectl apply -f frontend-deployment.yaml 
 kubectl apply -f reverseproxy-deployment.yaml
-kubectl apply -f pod-example/pod.yaml
+#kubectl port-forward service/reverseproxy 8080:8080
