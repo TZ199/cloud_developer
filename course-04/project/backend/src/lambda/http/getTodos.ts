658b0c5871eb36a17bb410dc172e499133dd9cae
jipsterious <cwtien@gmail.com>
2020-03-02 21:48:05 +0800

    commit

--- course-04/project/backend/src/lambda/http/getTodos.ts
+++ course-04/project/backend/src/lambda/http/getTodos.ts
@@ -3,5 +3,18 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  // TODO: Get all TODO items for a current user
+  sync getToDo(todoId: string, userId: string) {
+    const params = {
+        TableName: this.todoTable,
+        KeyConditionExpression: 'userId = :userId AND todoId = :todoId',
+        ExpressionAttributeValues: {
+            ':todoId': todoId,
+            ':userId': userId,
+        },
+        ScanIndexForward: false
+    };
+    const result = await this.docClient.query(params).promise();
+    const items = result.Items;
+    return items[0];
+}
 }
