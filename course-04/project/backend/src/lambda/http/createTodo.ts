bf90199c1110dfb39bc3b22097853912c69847bf
jipsterious <cwtien@gmail.com>
2020-03-07 23:48:39 +0800

    update 2020-03-07

--- course-04/project/backend/src/lambda/http/createTodo.ts
+++ course-04/project/backend/src/lambda/http/createTodo.ts
@@ -1,12 +1,25 @@
 import 'source-map-support/register'
-
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
-
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import * as uuid from 'uuid'
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+
+  const authorization = event.headers.Authorization
+  const split = authorization.split(' ')
+  const jwtToken = split[1]
+  const newItem = await createTodoRequest(newTodo, jwtToken)
+  
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      newItem
+    })
+  }
 }
--- course-04/project/backend/src/lambda/http/getTodos.ts
+++ course-04/project/backend/src/lambda/http/getTodos.ts
@@ -1,11 +1,14 @@
 import 'source-map-support/register'
-
+import * as aws from 'aws-sdk'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 
+const todoTable = process.env.TODOS_TABLE
+
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  sync getToDo(todoId: string, userId: string) {
+async getToDo(todoId: string, userId: string) => {
     const params = {
         TableName: this.todoTable,
+        IndexName: 
         KeyConditionExpression: 'userId = :userId AND todoId = :todoId',
         ExpressionAttributeValues: {
             ':todoId': todoId,
@@ -16,5 +19,16 @@ export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEven
     const result = await this.docClient.query(params).promise();
     const items = result.Items;
     return items[0];
-}
-}
+    }
+};
+
+await this.dynamoDBClient
+  .query({
+    TableName: 'table-name',
+    IndexName: 'index-name',
+    KeyConditionExpression: 'paritionKey = :paritionKey',
+    ExpressionAttributeValues: {
+      ':paritionKey': partitionKeyValue
+    }
+  })
+  .promise()
